fc5d86c2ebd76dd36b05e558de7c10f92116f34f
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-10-24 17:23:53 +0300

    add search

---
+++ pages/search.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+      <SearchHeader @searchPosts="posts = $event"/>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <nav aria-label="breadcrumb" class="mt-4">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><nuxt-link to="/">Главная</nuxt-link></li>
+                <li class="breadcrumb-item active" aria-current="page">Поиск</li>
+              </ol>
+            </nav>
+            <p class="lead">Найдено записей: {{posts.count}}</p>
+            <div v-for="post in posts.results" :key="post.id">
+              <nuxt-link :to="`/posts/${post.slug}`"><h2>{{ post.h1 }}</h2></nuxt-link>
+              <p v-html="post.description"></p>
+              <hr>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import SearchHeader from "@/components/SearchHeader";
+  import axios from "axios";
+  export default {
+    components: {SearchHeader},
+    layout: "post_detail",
+    watchQuery: ['q'],
+    data() {
+      return {
+        posts: ''
+      }
+    },
+    async asyncData({route}) {
+      const { data } = await axios.get(`http://localhost:8000/api/posts/?q=${route.query.q}`);
+      return {
+        posts: data,
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
